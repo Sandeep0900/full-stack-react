75ac2e43a1f8da68b8a8e6f456e40a543dea53af
Sandeep0900 <sandeesharma09@gmail.com>
2025-04-20 23:51:01 +0530

    updated App.js and deleted Component 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Component1 from './components/Component1';
-import Component2 from './components/Component2';
+// import Component2 from './components/Component2';
 import Component3 from './components/Component3';
 import Component4 from './components/Component4';
 import Login from './components/Login'; // Make sure it's capitalized!
@@ -21,7 +21,6 @@ const App = () => {
               <>
                 <div className="row">
                   <Component1 />
-                  <Component2 />
                 </div>
                 <div className="row">
                   <Component3 />
